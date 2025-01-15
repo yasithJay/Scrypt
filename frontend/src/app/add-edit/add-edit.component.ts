3a9bdf74da14b7207cf3f7c215566756c6ac8c40
Yasith Ketipearachchi <yasith@echonlabs.com>
2025-01-15 21:06:15 +0000

    home page frontend

---
+++ frontend/src/app/add-edit/add-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit',
+  imports: [],
+  templateUrl: './add-edit.component.html',
+  styleUrl: './add-edit.component.scss'
+})
+export class AddEditComponent {
+
+}
